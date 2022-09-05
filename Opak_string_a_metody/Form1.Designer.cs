78b5a86bbc2227e9283d2a07ef0c429ba24e6cec
dostalm.04 <dostalm.04@spst.eu>
2022-09-05 12:47:34 +0200

    hotovo

---
+++ Opak_string_a_metody/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Opak_string_a_metody
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 199);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Dnes   je pondělí, máme PRG.\r\nPetr    x2x    Novák\r\nPRG, MOV, MAT\r\nC1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(180, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 96);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "nejkratsi slova\r\nv řádku";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(180, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 97);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "První slovo s cifrou \r\na\r\nposlední slovo obsahující \r\n    zadaný podřetězec";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(345, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(118, 199);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(478, 226);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Opakování 3.ročník";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Opak_string_a_metody/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opak_string_a_metody
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocetRadku = textBox1.Lines.Count();
+            string[] p = new string[pocetRadku];
+            listBox1.Items.Clear();
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                string radek = textBox1.Lines[i];
+                char[] oddelovace = { ',', ' ', '.' };
+                string[] slova = radek.Split(oddelovace, StringSplitOptions.RemoveEmptyEntries);
+                string nejkratsiSlovo = slova[0];
+                for (int j = 0; j < slova.Length; j++)
+                {
+                    if (slova[j].Length < nejkratsiSlovo.Length)
+                    {
+                        nejkratsiSlovo = slova[j];
+                    }
+                }
+                p[i] = nejkratsiSlovo;
+                listBox1.Items.Add(p[i]);
+            }
+        }
+
+        void ZpracujPole(string[] pole, string podretezec, out string prvnisCifrou, out string posledniKonci)
+        {
+            prvnisCifrou = string.Empty;
+            posledniKonci = string.Empty;
+            bool nalCif = false;
+            for (int i = 0; i < pole.Length; i++)
+            {
+                string slovo = pole[i];
+                for (int j = 0; j < slovo.Length && !nalCif; j++)
+                {
+                    if (char.IsNumber(slovo[j]))
+                    {
+                        prvnisCifrou = slovo;
+                        nalCif = true;
+                    }
+                }
+                if (slovo.EndsWith(podretezec))
+                {
+                    posledniKonci = slovo;
+                }
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string[] pole = { "Bohdan", "S0S", "Jablko", "Oko","haf","aha", "Břenek" };
+            string podretezec = "ha";
+            string prvnisCifrou = string.Empty;
+            string posledniKonci = string.Empty;
+            ZpracujPole(pole, podretezec, out prvnisCifrou, out posledniKonci);
+            MessageBox.Show("První slovo obsahující cifru je: " + prvnisCifrou);
+            MessageBox.Show("Poslední řetězec končící zadaným podřetězcem je: " + posledniKonci);
+
+
+        }
+    }
+}
